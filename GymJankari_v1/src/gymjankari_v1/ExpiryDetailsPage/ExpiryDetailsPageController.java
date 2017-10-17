60a28b9df9dba85ac945c4a3c36ee4085a8b5438
aj33b <aj33b@aj33b-ThinkPad-X220>
2017-10-17 19:07:44 +0545

    Splash Loading Screen Added with other few changes.

---
+++ GymJankari_v1/src/gymjankari_v1/ExpiryDetailsPage/ExpiryDetailsPageController.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gymjankari_v1.ExpiryDetailsPage;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import gymjankari_v1.Main;
+import static gymjankari_v1.Main.mainLayout;
+import gymjankari_v1.ViewMemberMain.ViewMemberMainController;
+import gymjankari_v1.models.Member;
+import gymjankari_v1.service.MemberService;
+import gymjankari_v1.serviceimplementation.MemberServiceImplementation;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author aj33b
+ */
+public class ExpiryDetailsPageController implements Initializable {
+    
+    private Main main;
+    
+    @FXML
+    private RadioButton memberIdRadioButton;
+    @FXML
+    private RadioButton nameRadioButton;
+    @FXML
+    private JFXTextField searchTextField;
+    @FXML
+    private JFXButton searchButton;
+    @FXML
+    private TableView<Member> memberdetailTableView;
+    @FXML
+    private TableColumn<Member, String> memberidTableColumn;
+    @FXML
+    private TableColumn<Member, String> fullnameTableColumn;
+    @FXML
+    private TableColumn<Member, String> shiftTableColumn;
+    @FXML
+    private TableColumn<Member, String> phonenoTableColumn;
+    @FXML
+    private TableColumn<Member, String> expirydateTableColumn;
+    
+    /**
+     * Initializes the controller class.
+     * @param url
+     * @param rb
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        MemberService memberService = new MemberServiceImplementation();
+        populateTable();
+        memberdetailTableView.setItems(memberService.getAllMember());
+        memberdetailTableView.setOnMouseClicked(new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent event) {
+                FXMLLoader loader = new FXMLLoader();
+                try {
+                    loader.setLocation(Main.class.getResource("ViewMemberMain/ViewMemberMain.fxml"));
+                    BorderPane homepageLayout=loader.load();
+                    mainLayout.setCenter(homepageLayout);
+                } catch (IOException ex) {
+                    Logger.getLogger(ExpiryDetailsPageController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                ViewMemberMainController viewMemberController = loader.getController();
+                viewMemberController.setData(memberdetailTableView.getSelectionModel().getSelectedItem().getDisplayId());
+            }
+            
+        });
+    }    
+
+    @FXML
+    private void searchButtonClicked(ActionEvent event) {
+        MemberService memberService = new MemberServiceImplementation();
+        populateTable();
+        if(memberIdRadioButton.isSelected()){  
+            memberdetailTableView.setItems(memberService.searchById(searchTextField.getText()));
+        }else if(nameRadioButton.isSelected()){
+            memberdetailTableView.setItems(memberService.searchByName(searchTextField.getText()));
+        }else{ 
+            memberdetailTableView.setItems(memberService.getAllMember());
+        }  
+    }
+    
+    public void populateTable(){
+        memberidTableColumn.setCellValueFactory(new PropertyValueFactory<>("displayId"));
+        fullnameTableColumn.setCellValueFactory(new PropertyValueFactory<>("fullName"));
+        shiftTableColumn.setCellValueFactory(new PropertyValueFactory<>("shift"));
+        phonenoTableColumn.setCellValueFactory(new PropertyValueFactory<>("mobile"));
+        expirydateTableColumn.setCellValueFactory(new PropertyValueFactory<>("expiryDate"));
+    }
+    
+}
---
+++ GymJankari_v1/src/gymjankari_v1/MainView/SplashScreenController.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gymjankari_v1.MainView;
+
+import gymjankari_v1.Main;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.concurrent.Delayed;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.animation.FadeTransition;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
+
+/**
+ * FXML Controller class
+ *
+ * @author aj33b
+ */
+public class SplashScreenController implements Initializable {
+
+    private Main main;
+    @FXML
+    private ProgressBar loadingProgressBar;
+    @FXML
+    private StackPane root;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        if (!Main.issplashloaded){
+        loadSplashScreen();
+        }
+    }
+    
+    private void loadSplashScreen(){
+        try {
+            Main.issplashloaded=true;
+            StackPane splashscreenLayout=FXMLLoader.load(getClass().getResource("SplashScreen.fxml"));
+            root.getChildren().setAll(splashscreenLayout);
+            
+            
+            FadeTransition fadeIn= new FadeTransition(Duration.seconds(1),splashscreenLayout);
+            fadeIn.setFromValue(0);
+            fadeIn.setToValue(1);
+            fadeIn.setCycleCount(1);
+            
+            FadeTransition fadeOut= new FadeTransition(Duration.seconds(1),splashscreenLayout);
+            fadeOut.setFromValue(1);
+            fadeOut.setToValue(0);
+            fadeOut.setCycleCount(1);
+            
+            fadeIn.play();
+            
+            fadeIn.setOnFinished((e) ->{
+            fadeOut.play();
+            });
+            
+            fadeOut.setOnFinished((e)->{
+                try {
+                    main.showMainView();
+                    main.showviewmemberpage();
+                    } catch (IOException ex) {
+                    Logger.getLogger(SplashScreenController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            });
+            
+        } catch (IOException ex) {
+            Logger.getLogger(MainViewController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    
+}
+}
